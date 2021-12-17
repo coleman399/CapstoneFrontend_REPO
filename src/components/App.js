8cea8040f4f92353a7a44b5355b0b1e47f0dfa7d
Dillon Coleman <coleman399@gmail.com>
2021-12-17 12:55:18 -0700

    inital commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+            <p>HELLO WORLD</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
